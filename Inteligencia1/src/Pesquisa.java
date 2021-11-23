8a1c5a2cd076556b89dc7f61e673532554926196
Manuel-arc <mnltavares17@gmail.com>
2021-11-23 16:34:02 +0000

    Update Pesquisa.java

--- Inteligencia1/src/Pesquisa.java
+++ Inteligencia1/src/Pesquisa.java
@@ -6,6 +6,7 @@ public class Pesquisa {
         boolean resolve = false;
 
         ArrayList<Node> nosFronteira = new ArrayList<Node>();
+        //ArrayList<Node> estadoFechado = new ArrayList<Node>(); //Tirar comentario para usar estados fechados
 
         Node n = new Node(new PuzzleDeSeis(), null, null);
 
@@ -15,11 +16,21 @@ public class Pesquisa {
         int i = 0;
 
         while(!resolve){
+            //Para usar estado fechados
+            /*for(Node q : estadoFechado){
+                if(q.equals(nosFronteira.get(i).getEstado())){
+                    i++;
+                    continue;
+                }
+            }*/
+
             System.out.println(nosFronteira.get(i).getEstado());
             if(nosFronteira.get(i).getEstado().goal()){ 
                 resolve = true;
                 goal = nosFronteira.get(i);
-            }
+            } /*else{ // tirar comentario para usar estados fechados
+                estadoFechado.add(nosFronteira.get(i));
+            }*/
 
             nosFronteira.addAll(nosFronteira.get(i).getNosSucessores());
             System.out.println("Profundidade: "+ nosFronteira.get(i).getProfundidade());
