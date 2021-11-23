88e17813dbbe900bc7436711302255fb5daeec01
Manuel-arc <mnltavares17@gmail.com>
2021-11-23 17:12:01 +0000

    a

--- Inteligencia1/src/Pesquisa.java
+++ Inteligencia1/src/Pesquisa.java
@@ -6,7 +6,7 @@ public class Pesquisa {
         boolean resolve = false;
 
         ArrayList<Node> nosFronteira = new ArrayList<Node>();
-        //ArrayList<Node> estadoFechado = new ArrayList<Node>(); //Tirar comentario para usar estados fechados
+        ArrayList<Node> estadoFechado = new ArrayList<Node>(); //Tirar comentario para usar estados fechados
 
         Node n = new Node(new PuzzleDeSeis(), null, null);
 
@@ -16,20 +16,19 @@ public class Pesquisa {
 
         while(!resolve){
             //Para usar estado fechados
-            /*for(Node q : estadoFechado){
-                if(q.equals(nosFronteira.get(i).getEstado())){
-                    i++;
+            for(Node q : estadoFechado){
+                if(q.getEstado().equals(nosFronteira.get(0).getEstado())){
                     continue;
                 }
-            }*/
+            }
 
             System.out.println(nosFronteira.get(0).getEstado());
             if(nosFronteira.get(0).getEstado().goal()){ 
                 resolve = true;
                 goal = nosFronteira.get(0);
-            } /*else{ // tirar comentario para usar estados fechados
-                estadoFechado.add(nosFronteira.get(i));
-            }*/
+            } else{ // tirar comentario para usar estados fechados
+                estadoFechado.add(nosFronteira.get(0));
+            }
 
             nosFronteira.addAll(nosFronteira.get(0).getNosSucessores());
             System.out.println("Profundidade: "+ nosFronteira.get(0).getProfundidade());
@@ -38,6 +37,7 @@ public class Pesquisa {
 
         System.out.println("\nNó final:\n"+ goal.getEstado());
         System.out.println("Profundidade Final: "+ goal.getProfundidade());
+        System.out.println("Custo Final: "+ goal.g());
     }
 
     public static void pesquisaProfundidade(){
