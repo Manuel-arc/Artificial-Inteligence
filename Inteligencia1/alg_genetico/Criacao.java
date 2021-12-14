6237169ce9bc79ceed680fdd47dc95685368fc1f
Manuel-arc <mnltavares17@gmail.com>
2021-12-14 17:09:42 +0000

    .

---
+++ Inteligencia1/alg_genetico/Criacao.java
@@ -0,0 +1,5 @@
+public class Criacao {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Inteligencia1/alg_genetico/Individuo.java
@@ -0,0 +1,9 @@
+public class Individuo {
+
+    public Individuo(){
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
